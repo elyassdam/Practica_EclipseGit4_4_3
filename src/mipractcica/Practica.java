ef83d28bd9b7c7520322f95722cc006374444892
srtyo <srtyo@YASSSINGAMING>
2023-01-31 20:37:20 +0100

    Esto es un cambio

---
+++ src/mipractcica/Practica.java
@@ -0,0 +1,17 @@
+package mipractcica;
+import java.util.Arrays;
+
+public class Practica {
+	public static void main(String[] args) {
+		int numero[]=new int[10];
+        double numeros_decimales[]=new double[10];
+        boolean verdadero_falso[]=new boolean[5];
+System.out.println("numeros enteros"+Arrays.toString(numero));        
+System.out.println("numeros decimales"+Arrays.toString(numeros_decimales));        
+System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso));        
+
+    }
+    
+
+	}
+
