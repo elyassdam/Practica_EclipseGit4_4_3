5af10e9743870c0a6224c806453bd179c263e3d1
elyassdam <95480897+elyassdam@users.noreply.github.com>
2023-01-31 20:52:03 +0100

    Update1Practica.java
    En este cambio hemos añadido un array de strings y lo imprimimos.

--- src/mipractcica/Practica.java
+++ src/mipractcica/Practica.java
@@ -6,9 +6,13 @@ public class Practica {
 		int numero[]=new int[10];
         double numeros_decimales[]=new double[10];
         boolean verdadero_falso[]=new boolean[5];
+		String cadena[]=new String[2];
 System.out.println("numeros enteros"+Arrays.toString(numero));        
 System.out.println("numeros decimales"+Arrays.toString(numeros_decimales));        
-System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso));        
+System.out.println("verdadero o falso "+Arrays.toString(verdadero_falso));    
+System.out.println("cadena "+Arrays.toString(cadena));     
+
+		
 
     }
     
